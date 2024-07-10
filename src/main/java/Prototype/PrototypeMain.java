75106ac52a5f72e39c6ad7af67ac955e10c3d205
trixi7 <trixiemmanuelle.obar@gcash.com>
2024-07-10 13:31:09 +0800

    formatted

--- src/main/java/Prototype/PrototypeMain.java
+++ src/main/java/Prototype/PrototypeMain.java
@@ -7,10 +7,12 @@ class PrototypeMain {
             S1.getDetails();
             System.out.println(S1);
             System.out.println();
+
             SuperAlienCar S2 = (SuperAlienCar) S1.getClone();
             S2.setDriver("Grab");
             S2.getDetails();
             System.out.println(S2);
+
         } catch (Exception e) {
             System.out.println("whatsup");
         }
