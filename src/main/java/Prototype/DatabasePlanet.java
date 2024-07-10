b0654c1c714ff513aa5d821d7490ae6f4cf21305
Uzake88 <102020234+Uzake88@users.noreply.github.com>
2024-07-10 13:30:00 +0800

    "justin"

---
+++ src/main/java/Prototype/DatabasePlanet.java
@@ -0,0 +1,9 @@
+package Prototype;
+
+public class DatabasePlanet
+{
+    protected static String getPlanet() {
+        System.out.println("this operation took 1 minute");
+        return "Mars";
+    }
+}
---
+++ src/main/java/Prototype/SuperAlienCar.java
@@ -0,0 +1,18 @@
+package Prototype;
+
+public class SuperAlienCar extends SuperCarPlan {
+    private String planet;
+    private String alienDriver = "Lalamove"; // by default
+    public SuperAlienCar() {
+        this.planet = DatabasePlanet.getPlanet(); // costly operation
+    }
+    public void setDriver(String name) {
+        this.alienDriver = name;
+    }
+    public void getDetails() {
+        System.out.println("driver name is " + this.alienDriver);
+    }
+    public String toString(){
+        return "test";
+    }
+}
