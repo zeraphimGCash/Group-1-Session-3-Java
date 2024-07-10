908d3217819e3297e65e10640cd1161c356f6e4d
zeraphimGCash <john.diamante@gcash.com>
2024-07-10 12:09:35 +0800

    LegacyPrinterAdapter

---
+++ src/main/java/Adapter/LegacyPrinterAdapter.java
@@ -0,0 +1,16 @@
+package Adapter;
+
+class LegacyPrinterAdapter implements PaymentGateway {
+    private final LegacyPrinter legacyPrinter;
+
+    public LegacyPrinterAdapter(LegacyPrinter legacyPrinter) {
+        this.legacyPrinter = legacyPrinter;
+    }
+
+    @Override
+    public void processPayment(double amount) {
+        // Adapt the payment processing logic
+        legacyPrinter.printDocument();
+        System.out.println("Payment processed for ₱" + amount);
+    }
+}
