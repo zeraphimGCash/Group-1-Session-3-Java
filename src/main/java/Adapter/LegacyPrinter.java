a8c90880c323028063c577068e2d9896daf756a7
zeraphimGCash <john.diamante@gcash.com>
2024-07-10 12:15:19 +0800

    Added initialize method in LegacyPrinter

--- src/main/java/Adapter/LegacyPrinter.java
+++ src/main/java/Adapter/LegacyPrinter.java
@@ -1,8 +1,13 @@
 package Adapter;
 
 class LegacyPrinter {
+    void initialize() {
+        System.out.println("Initializing Printer ...");
+    }
+
     void printDocument() {
         // Some legacy printing logic
         System.out.println("Printing document...");
     }
+
 }
