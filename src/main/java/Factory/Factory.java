1ad936beb76770773363e00e79d232f27ae2372c
IsbakSilog <ca.cabahug@gmail.com>
2024-07-10 11:37:19 +0800

    Test push by isbaksilog2

--- src/main/java/Factory/Factory.java
+++ src/main/java/Factory/Factory.java
@@ -5,5 +5,6 @@ public class Factory {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("yo");
+        System.out.println("Test");
     }
 }
